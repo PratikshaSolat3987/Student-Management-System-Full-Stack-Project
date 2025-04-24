8a9eef2590425c7c32eaa2f832d9871b50b02a1c
PratikshaSolat3987 <145910376+PratikshaSolat3987@users.noreply.github.com>
2025-04-24 07:27:15 +0530

    Add files via upload

--- Angular19/student-management/src/app/services/student.service.ts
+++ Angular19/student-management/src/app/services/student.service.ts
@@ -1,1 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/student';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+  private apiUrl = 'http://localhost:8080/students';
+
+  constructor(private http: HttpClient) {}
+
+  getAllStudents(): Observable<Student[]> {
+    return this.http.get<Student[]>(`${this.apiUrl}/all`);
+  }
+
+  getStudentById(id: number): Observable<Student> {
+    return this.http.get<Student>(`${this.apiUrl}/${id}`);
+  }
+
+  getStudentByEmail(email: string): Observable<Student> {
+    return this.http.get<Student>(`${this.apiUrl}/email/${email}`);
+  }
+
+  registerStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(`${this.apiUrl}/register`, student);
+  }
+
+  updateStudent(id: number, student: Student): Observable<Student> {
+    return this.http.put<Student>(`${this.apiUrl}/update/${id}`, student);
+  }
+
+  deleteStudent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  }
+}
